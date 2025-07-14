5ebb6b40b5c901118953bb3b03701b0ca61dced9
tekmbilical <tekmbilical@gmail.com>
2025-07-14 21:12:42 +0530

    feat: updates

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true, // Helps catch issues in development
+  swcMinify: true,        // Enables faster minification using SWC
+}
+
+module.exports = nextConfig
